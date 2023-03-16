b0d6ad0106530729ae2d1cef3e9ee9fc73c0331c
liuhua <1670835059@qq.com>
2023-03-16 22:03:03 +0800

    feat: 轨道控制器查看物体

--- src/pages/laochen/base/demo2.vue
+++ src/pages/laochen/base/demo2.vue
@@ -3,5 +3,48 @@
   <div ref="box" />
 </template>
 <script setup>
-console.log(1111111);
+import { ref, onMounted } from "vue";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls";
+
+const box = ref();
+const init = () => {
+  // 1、创建场景
+  const scene = new THREE.Scene();
+
+  // 2、创建相机
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  // 设置相机位置
+  camera.position.set(0, 0, 10);
+  scene.add(camera);
+
+  // 3、创建物体
+  const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
+  const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+  // 根据几何体和材质创建物体
+  const cub = new THREE.Mesh(cubeGeometry, cubeMaterial);
+  scene.add(cub);
+
+  // 4、初始化渲染器
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth - 256, window.innerHeight);
+  box.value.appendChild(renderer.domElement);
+
+  // 使用轨道控制器
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  const animate = () => {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+  };
+  animate();
+};
+
+onMounted(() => init());
 </script>
